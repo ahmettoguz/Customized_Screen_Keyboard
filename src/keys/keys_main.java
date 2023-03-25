e9b2307a9b68ba91b42736346db7915506573683
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-03-26 01:22:25 +0300

    Project Upload.

---
+++ src/keys/keys_main.java
@@ -0,0 +1,16 @@
+package keys;
+
+import GUI.StartFrame;
+
+/*
+ * @author Ahmet Oğuz Ergin
+ */
+public class keys_main
+{
+    public static void main(String[] args)
+    {
+        StartFrame sf = new StartFrame();
+        sf.setVisible(true);
+    }
+    
+}
